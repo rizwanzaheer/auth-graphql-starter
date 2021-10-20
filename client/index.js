9ea161bd438148f4c8707260aa52e0731214f123
timothykempf35 <timothy.kempf35@gmail.com>
2021-10-20 21:38:36 +0500

    add root query, mutations, users types etc

--- client/index.js
+++ client/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient, { createNetworkInterface} from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+
+cosnt networkInterface = new createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+})
+
+const client = new ApolloClient({
+  dataIdFromObject: (o) => o.id,
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <div>Auth Starter</div>
+    </ApolloProvider>
   );
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<Root />, document.querySelector("#root"));
